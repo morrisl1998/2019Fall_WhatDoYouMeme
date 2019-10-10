4295e0d7c284f04f7008074d9773c5a6d4ad671f
Moshe Plotkin <mplotkin@jewpaltz.com>
2019-10-10 08:46:59 -0400

    made game controller

--- server/index.js
+++ server/index.js
@@ -1,27 +1,14 @@
 const express = require('express');
 const path = require('path');
 const userController = require('./controllers/Users');
+const gameController = require('./controllers/Game');
 
 const app = express();
 const port = 3000;
 
 app
-    .use( function( req, res, next) {
-        // Logging
-        
-        console.log( { params: req.params, body: req.body, url: req.url, query: req.query, headers: req.headers });
-        next();
-    })
     .use('/static', express.static( path.join( __dirname , '../NoFramework' ) ) )
-
-    .get(   '/',
-            (req, res) => res.send('Hello New Paltz!')
-        )
-    .get(   '/heb',
-        function(req, res){
-            res.send({ msg: 'Shalom World!' })
-        }
-    )
-    .use('/users', userController );
+    .use('/users', userController )
+    .use('/game', gameController );
 
 app.listen(port, () => console.log(`Running on http://localhost:${port}`));
