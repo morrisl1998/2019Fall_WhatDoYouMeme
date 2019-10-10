4546e95b32c305d603940e402081b952552a9d7c
Moshe Plotkin <mplotkin@jewpaltz.com>
2019-10-10 11:42:22 -0400

    using dynamic port

--- server/index.js
+++ server/index.js
@@ -4,9 +4,10 @@ const userController = require('./controllers/Users');
 const gameController = require('./controllers/Game');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT ||  3000;
 
 app
+    .get('/port', (req, res)=> res.send("Using port: " + port))
     .use('/static', express.static( path.join( __dirname , '../NoFramework' ) ) )
     .use('/users', userController )
     .use('/game', gameController );
