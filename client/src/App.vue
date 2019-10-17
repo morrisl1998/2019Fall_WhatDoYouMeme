9596e15aafadc216fde3ef96768a7d5a243cea0c
Moshe Plotkin <mplotkin@jewpaltz.com>
2019-10-16 22:00:01 -0400

    vue

---
+++ client/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app" class="container">
+    <mpHeader></mpHeader>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import mpHeader from "@/components/header.vue";
+
+export default ( {
+  components: { mpHeader }
+})
+</script>
+
+<style lang="scss">
+.blog-header {
+  line-height: 1;
+  border-bottom: 1px solid #e5e5e5;
+}
+
+.blog-header-logo {
+  font-family: "Playfair Display", Georgia, "Times New Roman", serif;
+  font-size: 2.25rem;
+}
+
+.blog-header-logo:hover {
+  text-decoration: none;
+}
+</style>
